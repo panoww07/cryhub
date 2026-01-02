ce6bda46c205031f2b85f87dddf3f9f84e4eba47
panoww07 <panoww2007@gmail.com>
2026-01-02 06:00:49 +0200

    Update cryhub.lua

--- cryhub.lua
+++ cryhub.lua
@@ -1,1 +1,18 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/panoww07/CryHub/main/cryhub.lua"))()
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+local animation = Instance.new("Animation")
+animation.AnimationId = "rbxassetid://97216011555871"
+humanoid.AnimationPlayed:Connect(function(track)
+track:Stop()
+end)
+RunService.Heartbeat:Connect(function()
+local playCount = math.random(30, 40)
+
+for i = 1, playCount do
+local track = humanoid:LoadAnimation(animation)
+track:Play()
+end
+end)
