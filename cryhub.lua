72751fe854a5223990e6964c48fa512ea892880e
panoww07 <panoww2007@gmail.com>
2026-01-02 06:30:51 +0200

    Update cryhub.lua

--- cryhub.lua
+++ cryhub.lua
@@ -1,46 +1,18 @@
--- CryHub Loader Script (Start / Stop)
-
+local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+local animation = Instance.new("Animation")
+animation.AnimationId = "rbxassetid://97216011555871"
+humanoid.AnimationPlayed:Connect(function(track)
+track:Stop()
+end)
+RunService.Heartbeat:Connect(function()
+local playCount = math.random(30, 40)
 
-local running = false
-local conn
-
-local function startLag()
-    if running then return end
-    running = true
-
-    conn = RunService.Heartbeat:Connect(function()
-        for i = 1, 1000 do
-            math.random()
-        end
-    end)
-
-    -- индикатор
-    local g = Instance.new("ScreenGui", game.CoreGui)
-    g.Name = "CryHubUI"
-    local t = Instance.new("TextLabel", g)
-    t.Size = UDim2.fromScale(1, 0.1)
-    t.TextScaled = true
-    t.Text = "CRYHUB RUNNING"
-    t.BackgroundColor3 = Color3.new(0, 0, 0)
-    t.TextColor3 = Color3.new(1, 1, 1)
-
-    warn("[CryHub] STARTED")
+for i = 1, playCount do
+local track = humanoid:LoadAnimation(animation)
+track:Play()
 end
-
-local function stopLag()
-    running = false
-    if conn then conn:Disconnect() conn = nil end
-
-    if game.CoreGui:FindFirstChild("CryHubUI") then
-        game.CoreGui.CryHubUI:Destroy()
-    end
-
-    warn("[CryHub] STOPPED")
-end
-
-getgenv().CryHub_Start = startLag
-getgenv().CryHub_Stop = stopLag
-
--- auto start
-startLag()
+end)
