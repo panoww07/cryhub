ccd01736380586f83e87ed141bfc48529cc85841
panoww07 <panoww2007@gmail.com>
2026-01-04 01:01:39 +0200

    Update cryhub.lua

--- cryhub.lua
+++ cryhub.lua
@@ -1,6 +1,5 @@
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
-
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
@@ -8,24 +7,22 @@ local humanoid = character:WaitForChild("Humanoid")
 local animation = Instance.new("Animation")
 animation.AnimationId = "rbxassetid://182436842"
 
+-- DO NOT stop the same animation you're forcing
 humanoid.AnimationPlayed:Connect(function(track)
-	track:Stop()
+	if track.Animation ~= animation then
+		track:Stop()
+	end
 end)
 
-RunService.RenderStepped:Connect(function()
-	local playCount = math.random(20, 50)
+RunService.Heartbeat:Connect(function()
+	local playCount = math.random(10, 25)
 
 	for i = 1, playCount do
 		local track = humanoid:LoadAnimation(animation)
-
 		track:Play()
-		track:AdjustSpeed(math.random(5, 15))
-		track.TimePosition = math.random() * 0.5
-
-		if math.random(1, 3) == 1 then
-			task.delay(math.random() * 0.2, function()
-				track:Stop()
-			end)
-		end
+		track:AdjustSpeed(math.random(6, 12))
 	end
+
+	task.wait(0.05) -- REQUIRED for executors
 end)
+
