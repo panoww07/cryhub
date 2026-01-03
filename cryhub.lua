435d5f7d991bfa713351f816d6aeb5655f465d02
panoww07 <panoww2007@gmail.com>
2026-01-04 01:04:04 +0200

    Update cryhub.lua

--- cryhub.lua
+++ cryhub.lua
@@ -1,5 +1,6 @@
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
+
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
@@ -7,22 +8,26 @@ local humanoid = character:WaitForChild("Humanoid")
 local animation = Instance.new("Animation")
 animation.AnimationId = "rbxassetid://182436842"
 
--- DO NOT stop the same animation you're forcing
-humanoid.AnimationPlayed:Connect(function(track)
-	if track.Animation ~= animation then
-		track:Stop()
-	end
-end)
+local tracks = {}
 
 RunService.Heartbeat:Connect(function()
-	local playCount = math.random(10, 25)
-
-	for i = 1, playCount do
+	-- random burst
+	for i = 1, math.random(5, 15) do
 		local track = humanoid:LoadAnimation(animation)
+		table.insert(tracks, track)
+
 		track:Play()
-		track:AdjustSpeed(math.random(6, 12))
+		track:AdjustSpeed(math.random(-10, 20))
+		track.TimePosition = math.random() * track.Length
 	end
 
-	task.wait(0.05) -- REQUIRED for executors
+	-- randomly kill tracks
+	for i = #tracks, 1, -1 do
+		if math.random() < 0.4 then
+			pcall(function()
+				tracks[i]:Stop()
+			end)
+			table.remove(tracks, i)
+		end
+	end
 end)
-
