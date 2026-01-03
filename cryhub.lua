230b0974e5475688a4c23b0ccc4ef4c85481e252
panoww07 <panoww2007@gmail.com>
2026-01-04 01:00:27 +0200

    Update cryhub.lua

--- cryhub.lua
+++ cryhub.lua
@@ -1,21 +1,31 @@
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
+
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
-local animation = Instance.new("Animation")
 
+local animation = Instance.new("Animation")
 animation.AnimationId = "rbxassetid://182436842"
 
 humanoid.AnimationPlayed:Connect(function(track)
 	track:Stop()
 end)
 
-RunService.Heartbeat:Connect(function()
-	local playCount = math.random(100, 200)
+RunService.RenderStepped:Connect(function()
+	local playCount = math.random(20, 50)
 
 	for i = 1, playCount do
 		local track = humanoid:LoadAnimation(animation)
+
 		track:Play()
+		track:AdjustSpeed(math.random(5, 15))
+		track.TimePosition = math.random() * 0.5
+
+		if math.random(1, 3) == 1 then
+			task.delay(math.random() * 0.2, function()
+				track:Stop()
+			end)
+		end
 	end
 end)
