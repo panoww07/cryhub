89fb3117feeef516d15052a9451c42f255d4e2e0
panoww07 <panoww2007@gmail.com>
2026-01-04 03:36:10 +0200

    Update cryhub.lua

--- cryhub.lua
+++ cryhub.lua
@@ -1,3 +1,4 @@
+
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
@@ -7,41 +8,53 @@ local active = false
 
 
 local confusionIds = {
-    "rbxassetid://507766388", -
+    "rbxassetid://507766388", 
     "rbxassetid://180435509", 
-    "rbxassetid://125750702", 
-    "rbxassetid://333349950", 
-    "rbxassetid://35654637"   
+    "rbxassetid://333349950"
 }
+
 
 UserInputService.InputBegan:Connect(function(input, gpe)
     if not gpe and input.KeyCode == Enum.KeyCode.K then
         active = not active
-        warn("Confusion Mode:", active)
+        warn("OVERDRIVE STATE:", active)
     end
 end)
 
-RunService.Heartbeat:Connect(function()
-    if not active then return end
 
-    local char = player.Character
-    local animator = char and char:FindFirstChildOfClass("Humanoid") and char.Humanoid:FindFirstChildOfClass("Animator")
+task.spawn(function()
+    while true do
+        if active then
+            local char = player.Character
+            local animator = char and char:FindFirstChildOfClass("Humanoid") and char.Humanoid:FindFirstChildOfClass("Animator")
 
-    if animator then
-        
-        for i = 1, 30 do 
-            local anim = Instance.new("Animation")
-            anim.AnimationId = confusionIds[math.random(1, #confusionIds)]
-            
-            local track = animator:LoadAnimation(anim)
-            
-            
-            track:Play(0.1, math.random(1, 10), math.random(5, 20))
-            
-            
-            track:Stop(0)
-            track:Destroy()
-            anim:Destroy()
+            if animator then
+                
+                for _, track in pairs(animator:GetPlayingAnimationTracks()) do
+                    track:Stop(0)
+                    track:Destroy()
+                end
+
+                
+                for i = 1, 45 do -- 
+                    local anim = Instance.new("Animation")
+                    anim.AnimationId = confusionIds[math.random(1, #confusionIds)]
+                    
+                    local track = animator:LoadAnimation(anim)
+                    
+                    
+                    
+                    track:Play(0, 10, 100) 
+                    
+                    
+                    task.spawn(function()
+                        task.wait()
+                        track:Destroy()
+                        anim:Destroy()
+                    end)
+                end
+            end
         end
+        task.wait(0.03) 
     end
 end)
