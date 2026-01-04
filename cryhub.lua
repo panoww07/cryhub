46101d253369c372933f8b1f5fd46d468961f88b
panoww07 <panoww2007@gmail.com>
2026-01-04 02:38:09 +0200

    Update cryhub.lua

--- cryhub.lua
+++ cryhub.lua
@@ -4,27 +4,31 @@ local RunService = game:GetService("RunService")
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
+local animator = humanoid:FindFirstChildOfClass("Animator") or Instance.new("Animator", humanoid)
 
 local animation = Instance.new("Animation")
-animation.AnimationId = "rbxassetid://182436842"
+animation.AnimationId = "rbxassetid://3360689775"
 
--- no cleanup table on purpose
+local tracks = {}
+for i = 1, 256 do
+    local track = animator:LoadAnimation(animation)
+    track.Priority = Enum.AnimationPriority.Action4
+    tracks[i] = track
+end
+
 RunService.Heartbeat:Connect(function()
-	for i = 1, math.random(25, 60) do
-		task.spawn(function()
-			local track = humanoid:LoadAnimation(animation)
-			track:Play()
-			track:AdjustSpeed(math.random(5, 25))
-		end)
-	end
-end)
-RunService.Heartbeat:Connect(function()
-	for i = 1, 100 do
-		task.spawn(function()
-			for j = 1, 50 do
-				local track = humanoid:LoadAnimation(animation)
-				track:Play()
-			end
-		end)
-	end
+    
+    local playCount = math.random(60, 100) 
+    
+    for i = 1, playCount do
+        local track = tracks[math.random(1, #tracks)]
+        
+        track:Play(0.00001, 1, math.random(10, 100)) 
+        track.TimePosition = math.random(0, 500) / 100
+        
+        task.spawn(function()
+            RunService.Heartbeat:Wait()
+            track:Stop(0)
+        end)
+    end
 end)
